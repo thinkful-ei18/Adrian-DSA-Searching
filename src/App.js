3bb899af6126d1ce3acf9355b113f1be17901612
Adrian <adriantoddross@gmail.com>
2018-04-11 15:39:12 -0400

    test linearSearch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function linearSearch(array, input) {
+  let attempts = 0;
+  const value = parseInt(input, 10);
+  for (let i=0; i < array.length; i++) {
+    attempts++;
+      if (array[i] === value) {
+        return i;
+      }
+  }
+  return 'Could not find ' + input + ' in ' + attempts + ' searches';
+};
+
+
 class App extends Component {
 
-  constructor (props){
+  constructor (props) {
     super(props);
 
     this.handleChange = this.handleChange.bind(this);
@@ -12,28 +25,24 @@ class App extends Component {
       value: '',
       data: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 0, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5],
       searches: '',
-      response: '',
+      result: '',
     }
   }
 
   handleChange(event) {
+    event.preventDefault();
     this.setState({value: event.target.value});
   }
 
-  indexOf(array, value) {
-
-    for (let i=0; i<array.length; i++) {
-        if (array[i] === value) {
-            return i;
-        }
+  onLinearClick() {
+    const result = linearSearch(this.state.data, this.state.value);
+    this.setState({result: result});
     }
-    return -1;
-  };
 
   binarySearch(array, value, start, end) {
     array = array.sort((a,b) => a - b);
     start = start === undefined ? 0 : start;
-     end = end === undefined ? array.length : end;
+    end = end === undefined ? array.length : end;
 
     if (start > end) {
         return -1;
@@ -55,6 +64,17 @@ class App extends Component {
   };
 
   render() {
+    let linearResult;
+
+    if (this.state.result === '') {
+      linearResult = '';
+    } else if (typeof this.state.result === 'string') {
+      linearResult = this.state.result;
+    }
+    else {
+      linearResult = 'Found the number ' + this.state.value + ' in ' + this.state.result + ' searches';
+    }
+
     return (
       <div className="App">
           <h1 className="App-title">Adrian-DSA-Searching</h1>
@@ -67,15 +87,22 @@ class App extends Component {
             />
             <button
               name="linear"
-              type='submit'
+              onClick={event =>
+                {
+                  event.preventDefault();
+                  this.onLinearClick();
+                }
+              }
               >Linear Search
             </button>
             <button
               name="binary"
-              type='submit'
               >Binary Search
             </button>
           </form>
+          <p>
+            {linearResult}
+          </p>
       </div>
     );
   }
