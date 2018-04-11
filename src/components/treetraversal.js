4e701bcdc1e777555b5743731ecabed595ba9ee5
Adrian <adriantoddross@gmail.com>
2018-04-11 17:02:58 -0400

    stub component

--- src/components/treetraversal.js
+++ src/components/treetraversal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../App.css';
 
 class treeTraversal extends Component {
   constructor(props) {
@@ -10,6 +10,7 @@ class treeTraversal extends Component {
     return(
       <div>
         <h2>Tree Traversal</h2>
+
       </div>
     );
   };
