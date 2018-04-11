35f0ce369a8967e75374f318f73c1dd2c10e17e7
Adrian <adriantoddross@gmail.com>
2018-04-11 13:55:45 -0400

    remove count

--- src/utils/linearsearch.js
+++ src/utils/linearsearch.js
@@ -1,8 +1,10 @@
 export default function indexOf(array, value) {
+
   for (let i=0; i<array.length; i++) {
       if (array[i] === value) {
           return i;
       }
+      // return console.log('count', count);
   }
   return -1;
 };
