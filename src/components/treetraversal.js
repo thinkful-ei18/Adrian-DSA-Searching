b2b4a2ce46bfb5768fbea80b4e427079961effa6
Adrian <adriantoddross@gmail.com>
2018-04-11 16:42:43 -0400

    add treeversal

---
+++ src/components/treetraversal.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class treeTraversal extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <h2>Tree Traversal</h2>
+      </div>
+    );
+  };
+}
+
+export default treeTraversal;
