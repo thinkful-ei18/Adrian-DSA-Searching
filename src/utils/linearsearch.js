09774d6c88e673883fae0f9ac83b45bb8e2db669
Adrian <adriantoddross@gmail.com>
2018-04-11 12:09:31 -0400

    fix ESLint violation: equality operator

--- src/utils/linearsearch.js
+++ src/utils/linearsearch.js
@@ -1,6 +1,6 @@
 export default function indexOf(array, value) {
   for (let i=0; i<array.length; i++) {
-      if (array[i] == value) {
+      if (array[i] === value) {
           return i;
       }
   }
