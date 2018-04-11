b6970c69b62f4c149c413a69e002501fe35a81a0
Adrian <adriantoddross@gmail.com>
2018-04-11 11:51:56 -0400

    add basic form functionality

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor (props){
+    super(props);
+    this.state = {
+      currentNumber: null
+    }
+  }
+
   render() {
+
+
     return (
       <div className="App">
           <h1 className="App-title">Adrian-DSA-Searching</h1>
           <form>
-            <input type="number" placeholder="Your Favorite Number"/>
-            <button type="submit">Linear Search</button>
-            <button type="submit">Binary Search</button>
+            <input
+              type="number"
+              placeholder="Your Favorite Number"
+              onChange={e => { e.preventDefault();
+              this.setState({currentNumber: e.target.value});
+            }}/>
+            <button
+              name="linear"
+              onClick={e => {e.preventDefault();
+              console.log('linear:', this.state.currentNumber);
+              }}
+              >Linear Search
+            </button>
+            <button
+              name="binary"
+              onClick={e => {e.preventDefault();
+                console.log('binary:', this.state.currentNumber);}}
+              >Binary Search
+            </button>
           </form>
       </div>
     );
